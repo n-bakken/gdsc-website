c5e5cec576f519b9b9c401e3f15659b7030e9677
HowardLee134 <m_lee134@u.pacific.edu>
2023-10-02 11:21:35 -0700

    3d model implement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import About from './components/pages/About';
 import ContactUs from './components/pages/ContactUs';
 import Homepage from './components/pages/Homepage';
--- src/index.js
+++ src/index.js
@@ -18,12 +18,15 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom'; // Changed import
 import App from './App';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const container = document.getElementById('root'); 
+const root = createRoot(container); // Use createRoot to initialize
+
+// Render the App using the new API
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
