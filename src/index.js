4e498aee9f4f239cef0b87681633f6b1267fdeae
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-10-12 14:53:32 -0700

    changes to package lock broke and fixed dependencies

--- src/index.js
+++ src/index.js
@@ -22,19 +22,6 @@ import { createRoot } from 'react-dom'; // Changed import
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 
-<<<<<<< HEAD
-export { default as Landing } from "./components/pages/Landing";
-export { default as Login } from "./components/pages/Login";
-export { default as Register } from "./components/pages/Register";
-
-ReactDOM.render(
-	<React.StrictMode>
-		<GoogleOAuthProvider clientId="857647722831-dksl94msme5t7sacimil60a57kshrnn4.apps.googleusercontent.com">
-			<App />
-		</GoogleOAuthProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
-=======
 const container = document.getElementById('root'); 
 const root = createRoot(container); // Use createRoot to initialize
 
@@ -45,6 +32,5 @@ root.render(
         <App />
       </GoogleOAuthProvider>
     </React.StrictMode>
-    document.getElementById('root')
->>>>>>> origin/master
+    , document.getElementById('root')
 );
