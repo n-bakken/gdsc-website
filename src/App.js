9ef87147d1cab299d4420b01a365f623c64e44b2
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-10-13 14:29:00 -0700

    removed google login stuff, ready to try new system

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/pages/About';
 import ContactUs from './components/pages/ContactUs';
 import Homepage from './components/pages/Homepage';
-import Login from './components/pages/Login';
 import "./App.css";
-import Register from './components/pages/Register';
-import Landing from './components/pages/Landing';
 
 const App = () => {
   return (
@@ -16,9 +13,6 @@ const App = () => {
         <Route path="/Home" element={<Homepage />} />
         <Route path="/About" element={<About />} />
         <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/Login" element={<Login  />} />
-        <Route path="/Register" element={<Register  />} />
-        <Route path="/Landing" element={<Landing  />} />
       </Routes>
     </Router>
   );
