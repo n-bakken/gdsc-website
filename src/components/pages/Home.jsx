113485cfd295ccebce82f696436f83e8c06e9682
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-10-13 21:13:28 -0700

    made navbar elements have an animation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -16,12 +16,12 @@ function Navbar() {
       <h1>Google Developer Club</h1>
       <img className="uoplogo" src={"images/uop.png"} width="5%" alt="gotigers" />
       <nav ref={navRef}>
-        <a href="/about">About</a>
-        <a href="/#">G Points Board</a>
-        <a href="/#">Events</a>
-        <a href="/ContactUs">Contact Us</a>
-        <a href="/Login">Login</a>
-        <a href="https://discord.gg/xrRTJsBukF" target="_blank" rel="noopener noreferrer" className="discord-button">Discord</a>
+        <div><a href="/about">About</a></div>
+        <div><a href="/#">G Points Board</a></div>
+        <div><a href="/#">Events</a></div>
+        <div><a href="/ContactUs">Contact Us</a></div>
+        <div><a href="/Login">Login</a></div>
+        <div><a href="https://discord.gg/xrRTJsBukF" target="_blank" rel="noopener noreferrer" className="discord-button">Discord</a></div>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
