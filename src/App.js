ae40ce9e54a5935755cd1b678123daba860d4f4d
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-10-15 15:02:02 -0700

    login and register page ui almost done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/pages/About';
 import ContactUs from './components/pages/ContactUs';
 import Homepage from './components/pages/Homepage';
+import {Login} from './components/pages/Login';
+import {Register} from './components/pages/Register';
 import "./App.css";
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/Home" element={<Homepage />} />
         <Route path="/About" element={<About />} />
         <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </Router>
   );
