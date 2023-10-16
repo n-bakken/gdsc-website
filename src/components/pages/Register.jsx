65038d9c1bb6c7c87c1775ae6f3016d428803601
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-10-15 18:40:11 -0700

    major stylistic overhaul

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -17,13 +17,13 @@ export const Register = () => {
         <div className="auth-form-container">
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
+            <label htmlFor="name">Full Name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Power Cat" />
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="p_cat@u.pacific.edu" id="email" name="email" />
+            <label htmlFor="password">Password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <button type="submit">Create my account</button>
         </form>
         <Link to="/Login">
         <button className="link-btn">Already have an account? Log in here</button>
