fcb1d1a9bf44bef037f572a0de34aa31fccc6c5f
Nora Bakken <norabakken@dhcp-10-15-181-152.wireless.pacific.edu>
2023-11-12 15:49:12 -0800

    website title dynamically changes based on screen size

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,67 +1,109 @@
-import React, { useRef, useEffect, useState} from "react";
-import { FaBars, FaTimes, FaCaretDown} from "react-icons/fa";
+import React, { useRef, useEffect, useState } from "react";
+import { FaBars, FaTimes, FaCaretDown } from "react-icons/fa";
 import "./css/Home.css";
 import { auth } from "../../firebase";
-import  Dropdown from "./Dropdown"; 
-
+import Dropdown from "./Dropdown";
 
 function Navbar() {
-    const navRef = useRef();
-    const [loginStatus, setLoginStatus] = useState(null);
-   
-    const [dropdown, setDropdown] = useState(false); 
-    const [click, setClick] = useState(false);
-   // const handleClick = () => setClick(!click);
-    
-    const onMouseEnter = () => {
-        if (window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(true);
-        }
-    };
+  const navRef = useRef();
+  const [loginStatus, setLoginStatus] = useState(null);
+  const [dropdown, setDropdown] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const onMouseLeave = () => {
-        if (window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(false);
-        }
-    };
-  
-    const showNavbar = () => {
-      navRef.current.classList.toggle("responsive_nav");
-    };
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-      if (auth.currentUser) {
-        setLoginStatus(1)
-      }
-    }, []);
-  
-    return (
-      <><header>
-        <h1><a href="/Home">Google Developer Student Club</a></h1>
-        <img className="uoplogo" src={"images/uop.png"} width="5%" alt="gotigers" />
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
+  const onMouseEnter = () => {
+    if (windowWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (windowWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      setLoginStatus(1);
+    }
+  }, []);
+
+  const title = windowWidth < 800 ? "GDSC" : "Google Developer Student Club";
+
+  return (
+    <>
+      <header>
+        <h1>
+          <a href="/Home" class="navtitle">{title}</a>
+        </h1>
+        <img
+          className="uoplogo"
+          src={"images/uop.png"}
+          width="5%"
+          alt="gotigers"
+        />
         <nav ref={navRef}>
-          <div><a href="/about">About</a></div>
-          <div><a href="/gpoints">G Points Board</a></div>
-          <div><a href="/Event">Events</a></div>
-          <div><a href="/ContactUs">Contact Us</a></div>
-          {loginStatus ? <div><a href="/Login">Logout</a></div> : <div><a href="/Login">Login</a></div>}
-          
-            <ul className= {click ? "nav-menu active" : "nav-menu"}> 
-                <li className="nav-item"
-                 onMouseEnter = { onMouseEnter}
-                 onMouseLeave = { onMouseLeave}
-                >
-                    <a href="/Resources" className="nav-links" > More <FaCaretDown /> </a>
-                    {dropdown && <Dropdown />}
-                </li>
-            </ul>
-          
-         
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <div>
+            <a href="/about">About</a>
+          </div>
+          <div>
+            <a href="/gpoints">G Points Board</a>
+          </div>
+          <div>
+            <a href="/Event">Events</a>
+          </div>
+          <div>
+            <a href="/ContactUs">Contact Us</a>
+          </div>
+          {loginStatus ? (
+            <div>
+              <a href="/Login">Logout</a>
+            </div>
+          ) : (
+            <div>
+              <a href="/Login">Login</a>
+            </div>
+          )}
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li
+              className="nav-item"
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <a href="/Resources" className="nav-links">
+                More <FaCaretDown />
+              </a>
+              {dropdown && <Dropdown />}
+            </li>
+          </ul>
+
+          <button
+            className="nav-btn nav-close-btn"
+            onClick={showNavbar}
+          >
             <FaTimes />
           </button>
         </nav>
@@ -69,20 +111,16 @@ function Navbar() {
           <FaBars />
         </button>
       </header>
-      </>
-    );
-  }
+    </>
+  );
+}
 
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
-
-  const navBar = () => {
-    return (
-      <div>
-         <Navbar />
-       
-      </div>
-    )
-  }
-
-  export default navBar; 
-  //export default Navbar;
+export default NavBar;
